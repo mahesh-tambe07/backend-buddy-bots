b179254836903ac8c5be9ecb7389dbff095b50e2
Mahesh Tambe <165809491+mahesh-tambe07@users.noreply.github.com>
2025-10-26 14:14:36 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ let db;
 
 if (!global.dbPool) {
   db = mysql.createPool({
-    host: process.env.DB_HOST,       // must be internal host
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -31,3 +31,39 @@ if (!global.dbPool) {
 }
 
 module.exports = db;
+
+
+
+// const mysql = require("mysql2/promise");
+// require("dotenv").config();
+
+// let db;
+
+// if (!global.dbPool) {
+//   db = mysql.createPool({
+//     host: process.env.DB_HOST,       // must be internal host
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+//   });
+
+//   (async () => {
+//     try {
+//       const conn = await db.getConnection();
+//       console.log("✅ MySQL connected successfully to", process.env.DB_HOST);
+//       conn.release();
+//     } catch (err) {
+//       console.error("❌ MySQL connection error:", err.message);
+//     }
+//   })();
+
+//   global.dbPool = db;
+// } else {
+//   db = global.dbPool;
+// }
+
+// module.exports = db;
